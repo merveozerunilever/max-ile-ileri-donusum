2becd0aa488464faf1b2d89d0fda829ae9efb025
merveozerunilever <106612806+merveozerunilever@users.noreply.github.com>
2022-07-18 13:53:27 +0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,9 @@ import BlogDetail1 from "./components/pages/maxBlog/BlogDetail1";
 import BlogDetail2 from "./components/pages/maxBlog/BlogDetail2";
 import BlogDetail3 from "./components/pages/maxBlog/BlogDetail3";
 import BlogDetail4 from "./components/pages/maxBlog/BlogDetail4";
+import BlogDetail5 from "./components/pages/maxBlog/BlogDetail5";
+import BlogDetail6 from "./components/pages/maxBlog/BlogDetail6";
+import BlogDetail7 from "./components/pages/maxBlog/BlogDetail7";
 
 
 export const routes = [
@@ -21,4 +24,7 @@ export const routes = [
   {name: "BlogDetail2", pathname: "//max-blog-oynamanin-gelecegi", element: BlogDetail2, isNav:false},
   {name: "BlogDetail3", pathname: "/max-blog-dongusel-hareket-ile-ileri-donusum", element: BlogDetail3, isNav:false},
   {name: "BlogDetail4", pathname: "/max-blog-mimari-reenkarnasyon-ve-ileri-donusum", element: BlogDetail4, isNav:false},
+  {name: "BlogDetail5", pathname: "/icerik-ileri-donusumu", element: BlogDetail5, isNav:false},
+  {name: "BlogDetail6", pathname: "/ileri-donusum-hayatin-kiyisinda-kalmislara-nasil-isik-oluyor", element: BlogDetail6, isNav:false},
+  {name: "BlogDetail7", pathname: "/ileri-donusum-kavramlari", element: BlogDetail7, isNav:false},
 ]
