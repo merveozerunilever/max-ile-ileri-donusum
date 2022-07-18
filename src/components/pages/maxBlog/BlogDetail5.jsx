cb3bc6d83bccdaac4ff0a5ae082626aa9f7aae23
merveozerunilever <106612806+merveozerunilever@users.noreply.github.com>
2022-07-18 13:54:27 +0300

    Add files via upload

---
+++ src/components/pages/maxBlog/BlogDetail5.jsx
@@ -0,0 +1,172 @@
+import "./BlogStyle.css";
+import {
+  blogBaslik1,
+  blogBaslik2,
+  blogBaslik4,
+  blogBaslik5,
+  blogBaslik6,
+  blogContentText4,
+  blogContentText41,
+  blogContentText42,
+  blogContentText43,
+  blogContentText44,
+  blogContentText45,
+  blogContentText46,
+  blogContentText47,
+  blogContentText48,
+  blogContentText49,
+  blogContentText50,
+  blogContentText51,
+  blogContentText52,
+  blogContentText53,
+  blogContentText54,
+  blogContentText55,
+  blogContentText56,
+  blogContentText57,
+  blogContentText58,
+  blogContentText59,
+  blogContentText591,
+  daimaGelisim,
+  ipucu,
+  madde1,
+  madde11,
+  madde2,
+  madde21,
+  madde3,
+  madde31,
+  madde4,
+  madde41,
+  RuhunuArayanYapılar,
+  tekIcerik,
+  yaraticiIcerik,
+  zamansizIcerik,
+  zenginIcgoru,
+} from "../../../constants";
+import { Card, CardGroup, Breadcrumb } from "react-bootstrap";
+
+import DocumentMeta from "react-document-meta";
+
+
+function BlogDetail() {
+  const meta = {
+    title: 'Anlatmanın 50 Yolu: İçerik İleri Dönüşümü | Max ile İleri Dönüşüm',
+    description: 'İleri dönüşüm ile ilgi blog yazılarımıza bu ekrandan ulaşabilirsiniz!',
+    canonical: "Self-Canonical",
+    meta: {
+        charset: 'utf-8',
+        name: {
+            keywords: 'react,meta,document,html,tags'
+        }
+    }
+  }
+  return (
+    <>
+<DocumentMeta {...meta} />
+      {/* <Card.Img
+        src="https://unlv-p-001-delivery.sitecorecontenthub.cloud/api/public/content/9b1185b21d4242cb97483cf2d2d30787?v=0407e75f"
+        alt="Anlatmanın 50 Yolu: İçerik İleri Dönüşümü"
+      /> */}
+                      <div className="yapi">
+<Breadcrumb className="firstItem">
+  <Breadcrumb.Item href="/">Manifesto</Breadcrumb.Item>
+  <Breadcrumb.Item href="/max-blog">MaxBlog</Breadcrumb.Item>
+  <Breadcrumb.Item active>Anlatmanın 50 Yolu: İçerik İleri Dönüşümü</Breadcrumb.Item>
+</Breadcrumb>
+</div>
+      <h1 className="blogTitleText">Anlatmanın 50 Yolu: İçerİk İleri Dönüşümü</h1>
+      <div className="blogBanner">
+        <img
+          className="blogAsset"
+          src="https://unlv-p-001-delivery.sitecorecontenthub.cloud/api/public/content/ede1a1dce307426d9729872fd8fa0e35?v=c3a2628e"
+          alt="Anlatmanın 50 Yolu: İçerik İleri Dönüşümü"
+        />
+        <div className="blogContentText">{blogContentText51}</div>
+        <div className="blog4">{yaraticiIcerik}</div>
+        <div className="blogContentText">{blogContentText52}</div>
+        <img
+          className="blogAsset"
+          src="https://unlv-p-001-delivery.sitecorecontenthub.cloud/api/public/content/1bfddb2b5f0049e6b1c87506b1c630cc?v=bd58403d"
+          alt="Anlatmanın 50 Yolu: İçerik İleri Dönüşümü"
+        />
+        <div className="blogContentText">{blogContentText53}</div>
+     
+        <div className="blog4">{zamansizIcerik}</div>
+        <div className="blogContentText">{blogContentText54}</div>
+
+        <img
+          className="blogAsset"
+          src="https://unlv-p-001-delivery.sitecorecontenthub.cloud/api/public/content/fbb995332e9e447fb008dd98c10d3b79?v=eebf4cb9"
+          alt="Zamansız İçerik"
+        />
+                <div className="blogContentText">{blogContentText55}</div>
+
+        <div className="blog4">{zenginIcgoru}</div>
+        <div className="blogContentText">{blogContentText56}</div>
+
+        <img
+          className="blogAsset"
+          src="https://unlv-p-001-delivery.sitecorecontenthub.cloud/api/public/content/e9db9b6d2f084febb6e665c2acee31aa?v=a68defdc"
+          alt="Zengin içgörülere sahip içerikler"
+        />
+                <div className="blogContentText">{blogContentText57}</div>
+
+        <div className="blog4">{daimaGelisim}</div>
+   
+        <div className="blogContentText">{blogContentText58}</div>
+        <div className="blogContentText">{blogContentText59}</div>
+        <div className="blog4">{tekIcerik}</div>
+        <div className="blogContentText">{blogContentText591}</div>
+        <div className="blogContentText">{madde1}</div>
+        <div className="blogContentText">{madde11}</div>
+        <div className="blogContentText">{madde2}</div>
+        <div className="blogContentText">{madde21}</div>
+        <div className="blogContentText">{madde3}</div>
+        <div className="blogContentText">{madde31}</div>
+        <div className="blogContentText">{madde4}</div>
+        <div className="blogContentText">{madde41}</div>
+
+      
+      </div>
+      <ul className="kaynakca">
+        Kaynakça
+        <ul>
+          <a
+            href={
+              "https://medium.com/the-anatomy-of-marketing/lets-talk-about-content-upcycling-e4ae2760e314"
+            } target="_blank"
+          >
+            {" "}
+            <li>
+            https://medium.com/the-anatomy-of-marketing/lets-talk-about-content-upcycling-e4ae2760e314
+            </li>
+          </a>
+          <a href={"https://www.thetilt.com/content/how-to-upcycle-content-marketing-success"} target="_blank">
+            {" "}
+            <li>
+            https://www.thetilt.com/content/how-to-upcycle-content-marketing-success
+            </li>{" "}
+          </a>
+          <a href={"https://meetedgar.com/blog/easy-ways-upcycle-content/"} target="_blank">
+            {" "}
+            <li>
+              https://meetedgar.com/blog/easy-ways-upcycle-content/
+            </li>{" "}
+          </a>
+          <a href={"https://vidooly.com/blog/ways-to-get-started-with-content-upcycling/"} target="_blank">
+            {" "}
+            <li>
+              https://vidooly.com/blog/ways-to-get-started-with-content-upcycling/
+            </li>{" "}
+          </a>
+          <a href={"https://www.lineagedigital.com/content-upcycling/"} target="_blank">
+            {" "}
+            <li>
+              https://www.lineagedigital.com/content-upcycling/
+            </li>{" "}
+          </a>
+        </ul>
+      </ul>
+    </>
+  );
+}
+export default BlogDetail;
